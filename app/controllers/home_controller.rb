b822668daae324b283e1525eb7491f51e01e5d70
MarioOimet <mario.oimet@gmail.com>
2017-11-08 07:26:58 -0600

    Added a controller to home view and am displaying Businesses with their enquiries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @busy = Business.all
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,5 @@
 class Business < ApplicationRecord
-  #has_many :contracts
-  #has_one :Enquiry
+  has_many :enquiries
   #has_one :Finance
   validates :name_business, :first_name_owner, :last_name_owner, :address_business, :city_business, :province_business, :postal_code_business, :phone_number_business, :e_mail_business, :pass_word, presence:true
   validates :pass_word, :length => { :in => 4..10 }
