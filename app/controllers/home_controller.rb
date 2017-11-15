d26ec58415f8fdbd90e56865abac0a710bd95915
MarioOimet <mario.oimet@gmail.com>
2017-11-14 20:11:41 -0600

    added kaminari gem and kaminari views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @busy = Business.all
+    @busy = Business.all.page(params[:page]).per(2)
   end
 
   def about
+      @info = Info.all
   end
 
   def contacts
+      @info = Info.all
   end
 end
